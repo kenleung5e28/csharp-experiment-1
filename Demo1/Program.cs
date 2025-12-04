6b21be4c7694d6b9dcb0ac0e1c48e336d769d2c4
Ken Leung <kenleung5e28@gmail.com>
2025-12-04 09:54:27 +0800

    Demo1 first commit

---
+++ Demo1/Program.cs
@@ -0,0 +1,26 @@
+﻿using HttpClient client = new();
+client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+var fetchTask1 = client.GetAsync("todos/1");
+var fetchTask2 = client.GetAsync("todos/8");
+try
+{
+    var results = await Task.WhenAll(fetchTask1, fetchTask2);
+    if (results.Any(r => !r.IsSuccessStatusCode))
+    {
+        Console.WriteLine("Some fetches failed.");
+        Environment.Exit(1);
+    }
+    var responses = await Task.WhenAll(results.Select(
+        r => r.Content.ReadAsStringAsync()
+        ));
+    Console.WriteLine("Fetch results:");
+    foreach (var response in responses)
+    {
+        Console.WriteLine(response);
+    }
+}
+catch (HttpRequestException e) {
+    Console.WriteLine("Fetch error occured:");
+    Console.WriteLine(e.Message);
+    Environment.Exit(1);
+}
