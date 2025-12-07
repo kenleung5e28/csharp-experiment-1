7ea8e0b535b085e9e5e36c52fc064b4b5d97cac9
Ken Leung <kenleung5e28@gmail.com>
2025-12-07 17:40:38 +0800

    new experiment wip

--- Demo2/Program.cs
+++ Demo2/Program.cs
@@ -1,33 +1,29 @@
-﻿static async Task RunLongTask(string name, int delay, CancellationToken token)
+﻿static async Task RunLongTask(CancellationToken token)
 {
-    Console.WriteLine("Task {0} started.", name);
-    await Task.Delay(delay, token);
-    Console.WriteLine("Task {0} completed.", name);
+    Console.WriteLine("The long task task started.");
+    await Task.Delay(3000, token);
+    Console.WriteLine("The long task completed.");
+}
+
+static async Task MustFailTask()
+{
+    Console.WriteLine("Must fail task started.");
+    throw new HttpRequestException("Must fail task: Unable to fetch data.");
+    Console.WriteLine("Must fail task completed.");
 }
 
 using var tokenSource = new CancellationTokenSource();
-tokenSource.CancelAfter(5000);
-List<(string, int)> taskDetails = [
-    ("boring", 7000), 
-    ("routine", 3000),
-    ("exciting", 1000), 
-];
-Dictionary<string, Task> tasks = taskDetails.ToDictionary(
-    k => k.Item1,
-    k => RunLongTask(k.Item1, k.Item2, tokenSource.Token)
-    );
+tokenSource.CancelAfter(1000);
+Task longTask = RunLongTask(tokenSource.Token);
+Task mustFailTask = MustFailTask();
+Console.WriteLine(longTask.Status);
+Console.WriteLine(mustFailTask.Status);
 try
-{
-    await Task.WhenAll(tasks.Values);
+{   
+    await Task.WhenAll(longTask, mustFailTask);
 }
-catch (OperationCanceledException)
+catch (Exception e)
 {
-    Console.WriteLine("Some tasks are canceled.");
-}
-var failedTaskNames = from t in tasks 
-    where !t.Value.IsCompletedSuccessfully 
-    select t.Key;
-foreach (string name in failedTaskNames)
-{
-    Console.WriteLine($"Task {name} failed.");
+    Console.WriteLine(e.GetType().Name);
+    Console.WriteLine(e.Message);
 }
