14e5e0961e8155a8450466175ff7b54f6f494b3b
Ken Leung <kenleung5e28@gmail.com>
2025-12-08 15:57:24 +0800

    update Demo2

--- Demo2/Program.cs
+++ Demo2/Program.cs
@@ -1,4 +1,11 @@
-﻿static async Task RunLongTask(CancellationToken token)
+﻿static async Task ShortTask(CancellationToken token)
+{
+    Console.WriteLine("Short task started.");
+    await Task.Delay(0, token);
+    Console.WriteLine("Short task completed.");
+}
+
+static async Task LongTask(CancellationToken token)
 {
     Console.WriteLine("The long task task started.");
     await Task.Delay(3000, token);
@@ -7,23 +14,45 @@
 
 static async Task MustFailTask()
 {
-    Console.WriteLine("Must fail task started.");
-    throw new HttpRequestException("Must fail task: Unable to fetch data.");
-    Console.WriteLine("Must fail task completed.");
+    await Task.Run(() =>
+    {
+        Console.WriteLine("Must fail task started.");
+        throw new HttpRequestException("Must fail task: Unable to fetch data.");
+        Console.WriteLine("Must fail task completed.");
+    });
 }
 
 using var tokenSource = new CancellationTokenSource();
 tokenSource.CancelAfter(1000);
-Task longTask = RunLongTask(tokenSource.Token);
+Task longTask = LongTask(tokenSource.Token);
 Task mustFailTask = MustFailTask();
-Console.WriteLine(longTask.Status);
-Console.WriteLine(mustFailTask.Status);
+Task shortTask = ShortTask(tokenSource.Token);
+Dictionary<string, Task> tasks = new (){
+    {nameof(longTask), longTask}, 
+    {nameof(mustFailTask), mustFailTask}, 
+    {nameof(shortTask), shortTask}
+};
 try
-{   
-    await Task.WhenAll(longTask, mustFailTask);
-}
-catch (Exception e)
 {
-    Console.WriteLine(e.GetType().Name);
-    Console.WriteLine(e.Message);
+    await Task.WhenAll(tasks.Values);
+}
+catch
+{
+    foreach (var entry in tasks)
+    {
+        string key = entry.Key;
+        Task task = entry.Value;
+        if (task.IsFaulted)
+        {
+            Console.WriteLine("{0}: faulted - {1}", key, task.Exception.Message);
+        }
+        else if (task.IsCanceled)
+        {
+            Console.WriteLine("{0}: canceled", key);
+        }
+        else if (task.IsCompletedSuccessfully)
+        {
+            Console.WriteLine("{0}: completed successfully", key);
+        }
+    }
 }
